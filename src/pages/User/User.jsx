afdbd8c6321ccbbb35734ae49664ce4f49e1f0b5
Priscouw <priscouw@gmail.com>
2024-10-18 15:53:44 +0200

    <feat> create form edit

--- src/pages/User/User.jsx
+++ src/pages/User/User.jsx
@@ -1,3 +1,4 @@
+// import { useDispatch } from "react-redux";
 import Account from "../../components/Account/Account";
 import Button from "../../components/Button/Button";
 import Footer from "../../layout/Footer/Footer";
@@ -17,6 +18,37 @@ const User = () => {
             Tony Jarvis!
           </h1>
           <Button className="edit-button" textContain="Edit Name" />
+          {/* <div className="user-info-content">
+            <h1>Edit user info</h1>
+            <form className="">
+              <div className="input-wrapper-user">
+                <label htmlFor="user-name">User name:</label>
+                <input type="text" id="user-name" />
+              </div>
+              <div className="input-wrapper-user">
+                <label htmlFor="firstname">First name:</label>
+                <input
+                  type="text"
+                  id="firstname"
+                  className="readonly"
+                  readOnly
+                />
+              </div>
+              <div className="input-wrapper-user">
+                <label htmlFor="lastname">Last name:</label>
+                <input
+                  type="text"
+                  id="lastname"
+                  className="readonly"
+                  readOnly
+                />
+              </div>
+              <div className="container-buttons">
+                <Button textContain="Save" className="button-large" />
+                <Button textContain="Cancel" className="button-large" />
+              </div> */}
+          {/* </form>
+          </div> */}
         </div>
         <h2 className="sr-only">Accounts</h2>
         <Account />
