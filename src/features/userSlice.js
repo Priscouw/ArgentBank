4ec255aee310ccf20536e30945390f9710c99fcb
Priscouw <priscouw@gmail.com>
2024-10-22 18:29:56 +0200

    <feat> create userSlice

---
+++ src/features/userSlice.js
@@ -0,0 +1,32 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+
+export const userSlice = createSlice({
+    name: "user",
+    initialState: {
+        email: "",
+        firstname: "",
+        lastname: "",
+        username: ""
+    },
+    reducers: {
+        get_infos: (state, action) => {
+            state.email = action.payload.body.email;
+            state.firstname = action.payload.body.firstName;
+            state.lastname = action.payload.body.lastName;
+            state.username = action.payload.body.userName;
+        },
+        remove_infos: (state) => {
+            state.email = ""
+            state.firstname = ""
+            state.lastname = ""
+            state.username = ""
+        }
+
+    }
+});
+
+
+export const { get_infos, remove_infos } = userSlice.actions;
+
+export default userSlice.reducer;
