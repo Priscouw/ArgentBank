70fac3ddee79934265dbd58fa75165674bb505e8
Priscouw <priscouw@gmail.com>
2024-10-22 10:56:20 +0200

    <refactor> remove password in state

--- src/features/loginSlice.js
+++ src/features/loginSlice.js
@@ -5,19 +5,16 @@ export const loginSlice = createSlice({
     name: "login",
     initialState: {
         username: "",
-        password: "",
         isLoggedIn: false,
     },
     reducers: {
         login: (state, action) => {
             state.username = action.payload.email;
-            state.password = action.payload.password;
             state.isLoggedIn = true;
         },
 
         logout: (state) => {
             state.username = "";
-            state.password = "";
             state.isLoggedIn = false;
         }
     }
