f7c889db897e6fb7b8c6c7db266795692a5ef451
Priscouw <priscouw@gmail.com>
2024-10-23 16:17:03 +0200

    <feat> add proptypes onclick

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -3,14 +3,19 @@ import "../Button/Button.scss";
 
 // La props className permet d'appliquer des styles css au bouton
 
-const Button = ({ textContain, className }) => {
+const Button = ({ textContain, className, onClick }) => {
   const buttonClass = `${className || ""}`;
-  return <button className={buttonClass}>{textContain}</button>;
+  return (
+    <button className={buttonClass} onClick={onClick}>
+      {textContain}
+    </button>
+  );
 };
 
 Button.propTypes = {
   textContain: PropTypes.string.isRequired,
   className: PropTypes.string,
+  onClick: PropTypes.bool,
 };
 
 export default Button;
