89f15d1077d9e4856f38057f25bf8236fcff15d6
Priscouw <priscouw@gmail.com>
2024-10-23 17:10:18 +0200

    <feat> add edit form toggle

--- src/pages/User/User.jsx
+++ src/pages/User/User.jsx
@@ -1,4 +1,5 @@
 import { useSelector } from "react-redux";
+import { useState } from "react";
 import Account from "../../components/Account/Account";
 import Button from "../../components/Button/Button";
 import Footer from "../../layout/Footer/Footer";
@@ -9,6 +10,13 @@ import "../User/User.scss";
 const User = () => {
   const firstname = useSelector((state) => state.user.firstname);
   const lastname = useSelector((state) => state.user.lastname);
+
+  const [editform, setEditForm] = useState(false);
+  const handleEdit = (e) => {
+    e.preventDefault();
+    setEditForm(!editform);
+  };
+
   return (
     <div>
       <MainNav />
@@ -19,38 +27,49 @@ const User = () => {
             <br />
             {firstname + " " + lastname}
           </h1>
-          <Button className="edit-button" textContain="Edit Name" />
-          {/* <div className="user-info-content">
-            <h1>Edit user info</h1>
-            <form className="">
-              <div className="input-wrapper-user">
-                <label htmlFor="user-name">User name:</label>
-                <input type="text" id="user-name" />
-              </div>
-              <div className="input-wrapper-user">
-                <label htmlFor="firstname">First name:</label>
-                <input
-                  type="text"
-                  id="firstname"
-                  className="readonly"
-                  readOnly
-                />
-              </div>
-              <div className="input-wrapper-user">
-                <label htmlFor="lastname">Last name:</label>
-                <input
-                  type="text"
-                  id="lastname"
-                  className="readonly"
-                  readOnly
-                />
-              </div>
-              <div className="container-buttons">
-                <Button textContain="Save" className="button-large" />
-                <Button textContain="Cancel" className="button-large" />
-              </div>
-            </form>
-          </div> */}
+          {editform ? (
+            <div className="user-info-content">
+              <h1>Edit user info</h1>
+              <form className="">
+                <div className="input-wrapper-user">
+                  <label htmlFor="user-name">User name:</label>
+                  <input type="text" id="user-name" />
+                </div>
+                <div className="input-wrapper-user">
+                  <label htmlFor="firstname">First name:</label>
+                  <input
+                    type="text"
+                    id="firstname"
+                    className="readonly"
+                    readOnly
+                  />
+                </div>
+                <div className="input-wrapper-user">
+                  <label htmlFor="lastname">Last name:</label>
+                  <input
+                    type="text"
+                    id="lastname"
+                    className="readonly"
+                    readOnly
+                  />
+                </div>
+                <div className="container-buttons">
+                  <Button textContain="Save" className="button-large" />
+                  <Button
+                    textContain="Cancel"
+                    className="button-large"
+                    onClick={handleEdit}
+                  />
+                </div>
+              </form>
+            </div>
+          ) : (
+            <Button
+              className="edit-button"
+              textContain="Edit Name"
+              onClick={handleEdit}
+            />
+          )}
         </div>
         <h2 className="sr-only">Accounts</h2>
         <Account />
