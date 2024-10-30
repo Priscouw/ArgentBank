8081aafde1714b4d711783021e047ed1e4f56827
Priscouw <priscouw@gmail.com>
2024-10-30 11:59:51 +0100

    <feat> add mainNav and footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,19 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Login from "./pages/Login/Login";
 import User from "./pages/User/User";
+import MainNav from "./layout/MainNav/MainNav";
+import Footer from "./layout/Footer/Footer";
 
 function App() {
   return (
     <BrowserRouter>
+      <MainNav />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/user" element={<User />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
